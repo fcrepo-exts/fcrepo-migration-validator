5e74cbb9da914669e9aa7c194ad0cb87a08dbdca
Andrew Woods <awoods@duraspace.org>
2020-12-14 17:12:11 -0500

    Add initial integration stubs
    - add initial F3/F6 test fixture

---
+++ src/test/java/org/fcrepo/migration/validator/RepoValidationIT.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.migration.validator;
+
+import org.junit.Test;
+
+/**
+ * @author awoods
+ * @since 2020-12-14
+ */
+public class RepoValidationIT extends AbstractValidationIT {
+
+    @Test
+    public void test() {
+
+    }
+
+
+}
