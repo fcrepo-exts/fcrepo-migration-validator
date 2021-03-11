26395c4e8ac74484684ceccf3fab2c3db6f28f86
Michael Ritter <emericaskate15@gmail.com>
2021-03-11 15:00:37 -0700

    Create tests for versioned datastreams

--- src/test/java/org/fcrepo/migration/validator/VersionValidationIT.java
+++ src/test/java/org/fcrepo/migration/validator/VersionValidationIT.java
@@ -17,6 +17,17 @@
  */
 package org.fcrepo.migration.validator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fcrepo.migration.validator.api.ValidationResult.ValidationType.BINARY_HEAD_COUNT;
+import static org.fcrepo.migration.validator.api.ValidationResult.ValidationType.BINARY_VERSION_COUNT;
+import static org.fcrepo.migration.validator.api.ValidationResult.ValidationType.SOURCE_OBJECT_RESOURCE_EXISTS_IN_TARGET;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.fcrepo.migration.validator.api.ValidationResult;
+import org.junit.After;
 import org.junit.Test;
 
 /**
@@ -25,8 +36,59 @@ import org.junit.Test;
  */
 public class VersionValidationIT extends AbstractValidationIT {
 
+    final File VERSIONS_BASE_DIR = new File(FIXTURES_BASE_DIR, "version-validation-it");
+
+    @After
+    public void teardown() {
+        FileUtils.deleteQuietly(RESULTS_DIR);
+    }
+
     @Test
     public void test() {
+        final var sourceObject = "1711.dl:FEG6WWJ664RHQ8X/DS1";
+        final var f3DatastreamsDir = new File(VERSIONS_BASE_DIR, "valid/f3/datastreams");
+        final var f3ObjectsDir = new File(VERSIONS_BASE_DIR, "valid/f3/objects");
+        final var f6OcflRootDir = new File(VERSIONS_BASE_DIR, "valid/f6/data/ocfl-root");
+        final var reportHandler = doValidation(f3DatastreamsDir, f3ObjectsDir, f6OcflRootDir);
+
+        // verify expected results
+        assertEquals("Should be no errors!", 0, reportHandler.getErrors().size());
+
+        // verify two entries for created date
+        final var numValidations = reportHandler.getPassed().stream()
+                     .filter(result -> result.getSourceObjectId().equals(sourceObject))
+                     .filter(result -> result.getDetails().contains("binary creation dates match"))
+                     .count();
+        assertEquals("Should be two created date validations for DS1", 2, numValidations);
+    }
+
+    @Test
+    public void testOcflMissingVersion() {
+        final var f3DatastreamsDir = new File(VERSIONS_BASE_DIR, "valid/f3/datastreams");
+        final var f3ObjectsDir = new File(VERSIONS_BASE_DIR, "valid/f3/objects");
+        final var f6OcflRootDir = new File(VERSIONS_BASE_DIR, "ocfl-fewer-versions/f6/data/ocfl-root");
+        final var reportHandler = doValidation(f3DatastreamsDir, f3ObjectsDir, f6OcflRootDir);
+
+        // verify expected results
+        final var errors = reportHandler.getErrors();
+        assertThat(errors).hasSize(3)
+                          .map(ValidationResult::getValidationType)
+                          .containsOnly(BINARY_VERSION_COUNT, BINARY_HEAD_COUNT,
+                                        SOURCE_OBJECT_RESOURCE_EXISTS_IN_TARGET);
+    }
+
+    @Test
+    public void testF3MissingVersion() {
+        final var f3DatastreamsDir = new File(VERSIONS_BASE_DIR, "valid/f3/datastreams");
+        final var f3ObjectsDir = new File(VERSIONS_BASE_DIR, "valid/f3/objects");
+        final var f6OcflRootDir = new File(VERSIONS_BASE_DIR, "ocfl-more-versions/f6/data/ocfl-root");
+        final var reportHandler = doValidation(f3DatastreamsDir, f3ObjectsDir, f6OcflRootDir);
+
+        // verify expected results
+        final var errors = reportHandler.getErrors();
+        assertThat(errors).hasSize(1)
+                          .map(ValidationResult::getValidationType)
+                          .containsOnly(BINARY_VERSION_COUNT);
 
     }
 
