85a5d3e9fa4a60c8c6826b3ddbeb2b8b2b2c5187
Michael Ritter <emericaskate15@gmail.com>
2021-04-29 11:50:41 -0600

    Add helper for checking if a F3 state is deleted in ocfl

--- src/main/java/org/fcrepo/migration/validator/impl/F3State.java
+++ src/main/java/org/fcrepo/migration/validator/impl/F3State.java
@@ -27,17 +27,36 @@ import org.fcrepo.migration.ObjectProperty;
 public enum F3State {
     DELETED, INACTIVE, ACTIVE;
 
+    /**
+     * Create a F3State from a String
+     * @param state the state
+     * @return
+     */
     public static F3State fromString(final String state) {
         switch (state.toUpperCase()) {
             case "A": case "ACTIVE": return ACTIVE;
             case "D": case "DELETED": return DELETED;
             case "I": case "INACTIVE": return INACTIVE;
-            default: throw new IllegalArgumentException("Invalid datastream state");
+            default: throw new IllegalArgumentException("Invalid Fedora 3 state");
         }
     }
 
+    /**
+     * Create a F3State from an ObjectProperty
+     * @param property the ObjectProperty
+     * @return
+     */
     public static F3State fromProperty(final ObjectProperty property) {
         return fromString(property.getValue());
+    }
+
+    /**
+     * Check if a F3State should be treated as Deleted in the Fedora 6 repository
+     * @param deleteInactive if Inactive state are Deleted
+     * @return true if the F3State is Deleted or deleteInactive is set and the state is Inactive
+     */
+    public boolean isDeleted(final boolean deleteInactive) {
+        return this == DELETED || (deleteInactive && this == INACTIVE);
     }
 
 }
